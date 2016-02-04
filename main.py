e6e0943ce8e5c24ba38761aeffdf015c813b5ff4
SorensenAndrewWPF <pumaomega@fullsail.edu>
2016-02-04 14:52:01 -0500

    "Hello" template working

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('main.html')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
