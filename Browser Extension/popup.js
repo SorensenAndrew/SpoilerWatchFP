cdbb48a79907d73499f10777996bb82e2df31d17
SorensenAndrewWPF <pumaomega@fullsail.edu>
2016-02-19 13:17:53 -0500

    Basic HTML scraping

--- Browser Extension/popup.js
+++ Browser Extension/popup.js
@@ -1,118 +1,56 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+$(document).ready(function() {
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    baseUrl = "http://www.mashable.com";
+    $.ajax({
+        url: baseUrl,
+        type: "get",
+        dataType: "",
+        success: function(data) {
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+            // load the response into jquery element
+            // form tags are needed to get the entire html,head and body
+            $foop = $('<form>' + data.responseText + '</form>');
+            //console.log(data.responseText);
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+            // find meta tags
+            $.each($foop.find("meta[content]"), function(idx, item) {
+                lnk = $(item).attr("content");
+                $('<option>' + lnk + '</option>').appendTo($('#meta'));
+            });
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+            // find links
+            $.each($foop.find('a[href]'), function(idx, item) {
+                lnk = $(item).attr("href");
+                $('<option>' + lnk + '</option>').appendTo($('#links'));
+            });
 
-    callback(url);
-  });
+            // find images bigger than 250x250
+            $.each($foop.find('img[src]'), function(idx, item) {
+                src = $(item).attr("src");
+                if (src.indexOf('http://') == -1) {
+                    src = baseUrl + src;
+                }
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
+                var img = new Image();
+                img.src = src;
+                img.onload = function() {
+                    //alert(this.width + 'x' + this.height);
+                    if (this.width > 250 && this.height > 250) {
+                        $(this).appendTo($('#images'));
+                    }
+                }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
+            });
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
+            // find contents of divs
+            $.each($foop.find('div'), function(idx, item) {
+                mytext = $(item).children().remove().text();
+                //$('<div>'+mytext+'</div>').appendTo($('#divs'));
+            });
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+        },
+        error: function(status) {
+            //console.log("request error:"+url);
+        }
     });
-  });
 });
---
+++ index.py
@@ -0,0 +1,183 @@
+from flask import Flask
+from flask import redirect
+from flask import request
+from flask import render_template
+from flask import session
+from flask import jsonify
+import urllib
+import json
+import pprint
+import hashlib
+import mysql.connector
+from urllib2 import urlopen
+from urllib2 import Request
+from json import load
+from lxml import html
+import requests
+
+app = Flask(__name__)
+app.secret_key = "4321"
+
+@app.route('/')
+def index():
+    return redirect('/login')
+
+
+@app.route('/login', methods=['post','get'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/users')
+def users():
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("select * from users")
+    data = cursor.fetchall()
+    return render_template('users.html',data=data)
+
+
+@app.route('/shows')
+def show():
+    name = session["username"]
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("select showName, showTitle, showSeason, showEpisode from showData where username='"+ name+ "'")
+    showdata = cursor.fetchall()
+    return render_template('shows.html',showdata=showdata)
+
+
+
+@app.route('/checklogin', methods=['post','get'])
+def checklogin():
+    session["username"] = request.form["username"]
+    session["password"] = request.form["password"]
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    hashval = hashlib.md5(request.form["password"]).hexdigest()
+
+    cursor.execute("select * from users where username=%s and password=%s", (session["username"], session["password"]))
+    data = cursor.fetchall()
+    if data:
+        data = {"username":session["username"],"password":session["password"]}
+        return render_template('showSearch.html',data=data)
+    else:
+        return redirect('/login')
+
+@app.route('/friendForm', methods=['post','get'])
+def friendForm():
+    return render_template('addFriend.html')
+
+@app.route('/addFriend', methods=['post','get'])
+def addfriend():
+    name = session["username"]
+    friend = request.form['Addfriend']
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("insert into friends(username1, username2)values(%s,%s)", (name, friend))
+    db.commit()
+    return redirect('/friends')
+
+@app.route('/friends')
+def friends():
+    name = session["username"]
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("select username1, username2 from friends where username1='"+ name+ "'")
+    data = cursor.fetchall()
+    return render_template('friends.html',data=data)
+
+@app.route('/friendData',methods=['post', 'get'])
+def frienddata():
+    friendname = request.form['friend']
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("select showName, showTitle, showSeason, showEpisode from showData where username='" + friendname + "'")
+    data = cursor.fetchall()
+    return render_template('friendData.html',data=data)
+
+
+@app.route('/newUser')
+def newUser():
+    return render_template('addUserform.html')
+
+
+@app.route('/addUser',methods=['post', 'get'])
+def addUser():
+    uname = request.form['newUser']
+    upass = request.form['newPassword']
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("insert into users(username, password)values(%s,%s)", (uname, upass))
+    db.commit()
+    return redirect('/login')
+
+
+@app.route('/parseJSON',methods=['post', 'get'])
+def parseJSON():
+    name = session["username"]
+    title = request.form["title"]
+    season = request.form['season']
+    episode = request.form['episode']
+    url = "http://www.omdbapi.com/?t=" + title +"&Season="+ season + "&Episode=" + episode +"&r=json"
+    url = url.replace(" ","%20")
+    loadurl = urllib.urlopen(url)
+    data = json.loads(loadurl.read().decode(loadurl.info().getparam('charset') or 'utf-8'))
+    episodedata = data['Episode']
+    titledata = data['Title']
+    seasondata= data['Season']
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("insert into showData(username, showTitle, showSeason, showEpisode, showName)values(%s,%s,%s,%s,%s)", (name, titledata, seasondata, episodedata, title))
+    db.commit()
+    return render_template('profilePage.html',data=data)
+
+
+@app.route('/showSearch')
+def nprForm():
+    return render_template('showSearch.html')
+
+
+@app.route('/deleteShow', methods=['post', 'get'])
+def deleteshow():
+    name = session["username"]
+    random = request.form["random"]
+    db = mysql.connector.connect(user='root', password='root',host='localhost', database='spoilerDB', port='8889')
+    cursor = db.cursor()
+    cursor.execute("delete from showData where showName='" + random + "' and username='" + name + "'")
+    db.commit()
+    return redirect('/shows')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/formtest', methods=['post','get'])
+def formtest():
+    userinput = request.form["user"]
+    hashed = hashlib.md5(userinput).hexdigest()
+    return hashed
+
+# HTML Scraping
+page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+tree = html.fromstring(page.content)
+
+#This will create a list of buyers:
+buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+#This will create a list of prices
+prices = tree.xpath('//span[@class="item-price"]/text()')
+
+print 'Buyers: ', buyers
+print 'Prices: ', prices
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
